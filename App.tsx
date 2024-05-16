47a43947db79dd37d28a236971b7a4da8bf95887
denisonfer <denisonferreiram91@gmail.com>
2024-05-15 23:11:42 -0300

    feat: added amplify aws

--- App.tsx
+++ App.tsx
@@ -1,5 +1,9 @@
 import Routes from './src/routes';
 
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from './src/amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);
+
 export default function App() {
   return <Routes />;
 }
